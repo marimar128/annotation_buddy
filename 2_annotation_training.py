b22613f943624990b6c400544414566aca11d3bf
marimar128 <marimar128@users.noreply.github.com>
2020-05-09 12:50:00 -0700

    Each class gets an equal vote

--- 2_annotation_training.py
+++ 2_annotation_training.py
@@ -12,8 +12,6 @@ assert torch.cuda.is_available(), 'No GPU available'
 
 num_input_channels = 1
 num_labels = 4 # Including 0, the "unannotated" label
-label_weights = torch.cuda.FloatTensor([1, 1, 1])
-assert len(label_weights) == num_labels - 1 # No weight for "unannotated"
 assert num_labels < 2**8 # Bro you don't need more
 model = torchvision.models.segmentation.fcn_resnet50(
     num_classes=num_labels - 1, # Only guess the annotated classes
@@ -23,8 +21,8 @@ model.backbone.conv1 = nn.Conv2d( # Input probably isn't RGB
     64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 model = model.cuda()
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2, amsgrad=True,
-                              weight_decay=1e-3)
+optimizer = torch.optim.AdamW(
+    model.parameters(), lr=1e-2, amsgrad=True, weight_decay=1e-3)
 
 saved_state_filename = './saved_model.pt'
 backup_saved_state_filename = './backup_saved_model.pt'
@@ -37,13 +35,14 @@ if os.path.exists(saved_state_filename):
     starting_epoch = 1 + checkpoint['epoch']
     model.train()
 
-def loss_fn(output, target, num_annotated_pixels):
+def loss_fn(output, target):
     # Our target has an extra slice at the beginning for unannotated pixels:
     output, target = output['out'], target[:, 1:, :, :]
     assert output.shape == target.shape
+    annotated_pixels_per_label = torch.sum(target, dim=(2, 3), keepdim=True)
+    label_weight = 1e-9 + (num_labels - 1) * annotated_pixels_per_label
     probs = F.softmax(output, dim=1)
-    probs = probs * label_weights.reshape(1, probs.shape[1], 1, 1)
-    return torch.sum(probs * target / -(num_annotated_pixels + 1e-9))
+    return torch.sum(probs * target / (-label_weight))
 
 img_dir = './2_human_annotations'
 
@@ -63,7 +62,6 @@ def load_data(img_name):
     # "1-hot" representation called 'target'.
     labels = img[np.newaxis, -1:, :, :].astype('uint8')
     assert labels.max() < num_labels
-    num_annotated_pixels = np.count_nonzero(labels)
     # Might as well pass a small dtype to the GPU, but the on-GPU dtype
     # has to be Long to work with .scatter_:
     labels = torch.cuda.LongTensor(labels)
@@ -73,7 +71,7 @@ def load_data(img_name):
         ).zero_() # Initialization to zero is not automatic!
     # Copy each class into its own boolean image:
     target.scatter_(dim=1, index=labels.data, value=True)
-    return input_, target, num_annotated_pixels
+    return input_, target
 
 def save_output(output, filename, dir_='./3_machine_annotations'):
     guess = F.softmax(output['out'].cpu().data, dim=1).numpy().astype('float32')
@@ -90,9 +88,9 @@ for epoch in range(starting_epoch, 100000): # Basically forever
     print("\nEpoch", epoch)
     for i, img_name in enumerate(img_names):
         print('.', sep='', end='')
-        input_, target, num_annotated_pixels = load_data(img_name)
+        input_, target = load_data(img_name)
         output = model(input_)
-        loss = loss_fn(output, target, num_annotated_pixels)
+        loss = loss_fn(output, target)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
